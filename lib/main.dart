47fe453a924e53241b91d0f1367058700dfde2c0
braian-john <braianjonh@hotmail.com>
2024-02-27 21:09:44 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:naotemintuito/home_page.dart';
+
+void main(){
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false,
+    home: HomePage()
+  ));
+}
